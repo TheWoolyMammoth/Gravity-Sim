a750705b66eaf0bb1cef24464672bdb6c019a416
Joey Waner <Joseph.Waner@wsu.edu>
2017-11-27 00:58:12 -0800

    minor changes

--- Gravity Sim/main.cpp
+++ Gravity Sim/main.cpp
@@ -2,11 +2,28 @@
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(500, 500), "Gravity Sim");
+	sf::RenderWindow window(sf::VideoMode(500, 500), "Gravity Sim");//declaring size of window, and setting the title
+
 	while (window.isOpen())//while the window is open
 	{
+
 		window.display();//open the window
+
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+			{
+				window.close();//closing window
+			}
+			//instantiate object
+			//movement
+			//speed of object?
+		}
+
 	}
-	window.clear();//clear the window
+
+	
+
 	return 0;
 }
