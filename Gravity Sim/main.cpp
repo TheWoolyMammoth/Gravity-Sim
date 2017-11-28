5b8b93f0cc729e76c57eefef84ac635b02cc6d58
Joey Waner <Joseph.Waner@wsu.edu>
2017-11-28 02:48:46 -0800

    Updates
    readme

--- Gravity Sim/main.cpp
+++ Gravity Sim/main.cpp
@@ -5,29 +5,50 @@ using std::cin;
 using std::cout;
 using std::endl;
 
+int buttonPress = 0;
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(500, 500), "Gravity Sim");//declaring size of window, and setting the title
-	Ball sphere(sf::Color::White, 1, *(new sf::Vector2f(window.getSize().x / 2, window.getSize().y / 2)));//instantiate sphere
+
 	while (window.isOpen())//while the window is open
 	{
-
-		window.display();//open the window
-
+		
+		//window.display();//open the window
+		
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
+			
 			if (event.type == sf::Event::Closed)
 			{
 				window.close();//closing window
 			}
-			
+			if (((sf::Mouse::getPosition(window).x > 0) && (sf::Mouse::getPosition(window).x < window.getSize().x-10)) && (sf::Mouse::getPosition(window).y > 0 && sf::Mouse::getPosition(window).y < window.getSize().y-10))//if local mouse pos is in bounds of window dimensions less the radius so it doesn't go over the bounding wall
+			{
+				if (sf::Mouse::isButtonPressed(sf::Mouse::Left) && buttonPress == 0)//if mouse has been clicked once and the left mouse button is clicked
+				{
+					Ball sphere(sf::Color::White, 10, *(new sf::Vector2f(sf::Mouse::getPosition(window).x,sf::Mouse::getPosition(window).y)));//instantiate sphere
+					
+					window.draw(sphere);
+					window.setVerticalSyncEnabled(true);
+					buttonPress++;
+				}
+				//else if (sf::Mouse::isButtonPressed(sf::Mouse::Left) && buttonPress == 1)
+				//{
+				//this is for moving the object after initial placement
+				//buttonPress++;
+				//}
+				
+			}
+			//left mouse button is clicked
+			//return the position of mouse, draw circle at that pos. 
+				//do we want to be able to control the object initial placement? - yes
 			//movement
 			//vector movement of object
-			//if screen clicked place ball
-			//window.draw(sphere);
+			
+			
 		}
-		
+		window.display();//open the window
 	}
 
 	
--- README.txt
+++ README.txt
@@ -5,13 +5,14 @@ Teammates: Joey Waner
 Objective: create a gravity/physics simulator
 
 Goals/Requirements
-1) spawnable object(initially a ball?)
-2) spawn on screen with a click of mouse
+1) spawnable object(initially a ball?)-object is spawnable anywhere but flashes. possible framerate issue?
+2) spawn on screen with a click of mouse - yes. 
 3) be able to set vector
 4) have bounding walls 
-5) ball must bounce off of walls
+5) ball(object) must bounce off of walls
 6) be able to adjust gravity
  
 Changelog:
            11/26/17 renamed ball.h/ball.cpp to moveable.h/moveable.cpp
                     generated class in .h file
+	   11/28/27 ball is spawnable in window with click of left mouse button
